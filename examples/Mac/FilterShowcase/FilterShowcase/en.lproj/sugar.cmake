fa4b269ef6bf81e2e8f82213685a318ea8440a42
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-04-14 18:44:31 +0200

    Add example: Mac/FilterShowcase

---
+++ examples/Mac/FilterShowcase/FilterShowcase/en.lproj/sugar.cmake
@@ -0,0 +1,17 @@
+# This file generated automatically by:
+#   generate_sugar_files.py
+# see wiki for more info:
+#   https://github.com/ruslo/sugar/wiki/Collecting-sources
+
+if(DEFINED FILTERSHOWCASE_FILTERSHOWCASE_EN_LPROJ_SUGAR_CMAKE_)
+  return()
+else()
+  set(FILTERSHOWCASE_FILTERSHOWCASE_EN_LPROJ_SUGAR_CMAKE_ 1)
+endif()
+
+include(sugar_files)
+
+sugar_files(
+    FILTERSHOWCASE_RESOURCES
+    MainMenu.xib
+)
---
+++ examples/Mac/FilterShowcase/sugar.cmake
@@ -0,0 +1,26 @@
+# This file generated automatically by:
+#   generate_sugar_files.py
+# see wiki for more info:
+#   https://github.com/ruslo/sugar/wiki/Collecting-sources
+
+if(DEFINED FILTERSHOWCASE_SUGAR_CMAKE_)
+  return()
+else()
+  set(FILTERSHOWCASE_SUGAR_CMAKE_ 1)
+endif()
+
+include(sugar_files)
+include(sugar_include)
+
+sugar_include(FilterShowcase)
+
+sugar_files(
+    FILTERSHOWCASE_SOURCES
+    SLSFilterShowcaseWindowController.h
+    SLSFilterShowcaseWindowController.m
+)
+
+sugar_files(
+    FILTERSHOWCASE_RESOURCES
+    SLSFilterShowcaseWindowController.xib
+)
